898694fc328a585d09c650abc566ddd6c0c2eb23
phg <philipp.gerth@gmx.net>
2017-08-16 14:01:21 +0200

    webApp map added quarries

--- webApp/src/app/component/map.component.ts
+++ webApp/src/app/component/map.component.ts
@@ -10,6 +10,8 @@ import { ResourcesService } from '../service/resources.service';
   styles: ['./map.component.css', '../../node_modules/leaflet/dist/leaflet.css'],
 })
 export class MapComponent {
+  public places: Object[];
+  public map: L.Map;
 
   constructor(
     private resourcesService: ResourcesService,
@@ -18,16 +20,37 @@ export class MapComponent {
 
   ngOnInit(): void {
     // initialize map & mapping parameters
-    let map = new L.Map('map', {
+    this.map = L.map('map', {
       center: new L.LatLng(40, 15),
+      zoomControl: false,
       zoom: 5,
       layers: [this.mapService.baseMaps.RomanEmpire],
     });
 
     // adds map control: layertree, scale
     let baseMaps = this.mapService.baseMaps;
-    L.control.layers(baseMaps).addTo(map);
-    L.control.scale().addTo(map);
+    L.control.zoom({position: 'topright'}).addTo(this.map);
+    L.control.layers(baseMaps).addTo(this.map);
+    L.control.scale().addTo(this.map);
 
+    this.resourcesService
+      .getPlaces()
+      .subscribe((places: any) => {
+        this.places = places;
+        for (let place of places) {
+          this.genrateMarkerForPlace(place);
+        }
+      });
+
+  }
+
+  private genrateMarkerForPlace (place : any) {
+    L.marker([place._source.location.lat, place._source.location.lon])
+      .addTo(this.map)
+      .bindPopup(
+        "<b>" + place._source['dcterms:title'] + "</b><br>" +
+        place._source['dcterms:description'] + "<br>" +
+        "<a href=" + place._source['@id'] + ">" + place._source['@id'] + "</a>"
+      );
   }
 }
